4c101bcbb5f0b6be2d3d20f4cdd483a8fd5c1580
Juma Samuel <jumasamuel269@gmail.com>
2023-03-28 12:33:27 +0300

    Test Completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const currentUser = 'Tomas Soucek';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
 // Write your code in this file!
